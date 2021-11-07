c8b4c238193da1354fa7688bb3c55c6691678286
kevans107 <kellyevans107.gmail.com>
2021-11-07 16:03:54 -0500

    header, footer, and home components created with some styling

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,31 +1,25 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Footer from './components/Footer'
 
+import {
+  BrowserRouter as  Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 class App extends Component {
-  render () {
-    const {
-      logged_in,
-      current_user,
-      new_user_route,
-      sign_in_route,
-      sign_out_route
-    } = this.props
+  render() {
     return (
-      <>
-      <h1>Hello World</h1>
-      { logged_in &&
-          <div>
-            <a href={sign_out_route }>Sign Out</a>
-          </div>
-        }
-        { !logged_in &&
-          <div>
-            <a href={ sign_in_route }>Sign In</a>
-          </div>
-        }
-      </>
-    );
+      <Router>
+        <Header {...this.props} />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <Footer {...this.props} />
+      </Router>
+    )
   }
 }
-
 export default App
---
+++ app/javascript/components/pages/Home.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import homeImage from '../assets/homeimage.png'
+import petImage from '../assets/pet.png'
+import fitnessImage from '../assets/fitness-center.png'
+import patioImage from '../assets/patio.png'
+import applianceImage from '../assets/appliances.png'
+import wifiImage from '../assets/wifi.png'
+
+class Home extends Component {
+    render(){
+        return(
+            <>
+            <div className="container">
+             <img
+                src={homeImage}
+                alt="vertical forest apartments in Milan"
+                className="home-image"
+             />
+             <div className="home-body">
+                 <h1 className="home-header">Tree Top Apartments</h1>
+                 <br/>
+                 <div className="home-row">
+                     <div className="home-column">
+                     <img src={petImage}alt="Pet Friendly icon" className="home-icons"/>
+                     </div>
+                     <div className="home-column">
+                     <img src={fitnessImage}alt="Fitness Center icon" className="home-icons"/>
+                     </div>
+                     <div className="home-column">
+                     <img src={patioImage}alt="Patio and Balcony icon" className="home-icons"/>
+                     </div>
+                     <div className="home-column">
+                     <img src={applianceImage}alt="Stainless Steel Appliances icon" className="home-icons"/>
+                     </div>
+                     <div className="home-column">
+                     <img src={wifiImage}alt="wifi icon" className="home-icons"/>
+                     </div>
+                 </div>
+                 <h4 className="home-text">Elevate your life with the ideal blend of urban convenience and sustainable living.</h4>
+             </div>
+             </div>
+            </>
+        ) 
+    }
+    
+}
+
+export default Home
